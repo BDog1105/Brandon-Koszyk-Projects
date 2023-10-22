10025c93ff84ea76caeec192029e2ef4ea6ef267
Bman1105 <112725723+Bman1105@users.noreply.github.com>
2023-10-22 00:49:00 -0400

    removed previous login for dropdown equivalent

--- client/src/components/LoginBadge.vue
+++ client/src/components/LoginBadge.vue
@@ -8,8 +8,12 @@
     login('hbingley1@plala.or.jp', 'CQutx25i8r')
   }
 
-  const doLogin = () => {
-    login('hbingley1@plala.or.jp', 'CQutx25i8r')
+  const doLogin1 = () => {
+    login('shaq@nba.com', 'Magic')
+  }
+
+  const doLogin2 = () => {
+    login('quadale@dingle', 'rehehehehe')
   }
 
   const doLogout = () => {
@@ -34,11 +38,28 @@
     <a class="button is-primary">
       <strong>Sign up</strong>
     </a>
-    <a class="button is-light" @click.prevent="doLogin">
-      Log in
-    </a>
+    <div class="dropdown is-active" data-v-1f0da592="">
+                                        <div class="dropdown-trigger">
+                                            <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+                                                <span data-v-1f0da592="">Log in</span>
+                                                <span class="icon is-small" data-v-1f0da592="">
+                                                    <i class="fas fa-angle-down" aria-hidden="true" data-v-1f0da592=""></i>
+                                                </span>
+                                            </button>
+                                        </div>
+                                        <div class="dropdown-menu" id="dropdown-menu" role="menu">
+                                            <div class="dropdown-content">
+                                                <a class="dropdown-item" data-v-1f0da592=""> Moshe </a>
+                                                <a class="dropdown-item" data-v-1f0da592=""> Kamala Harris </a>
+                                                <a class="dropdown-item" data-v-1f0da592=""> John Smith </a>
+                                                <hr class="dropdown-divider" data-v-1f0da592="">
+                                                <a href="/login" class="router-link-active router-link-exact-active dropdown-item is-disabled" data-v-1f0da592="" aria-current="page"> Other Account </a>
+                                            </div>
+                                        </div>
+                                    </div>
   </div>
 </template>
+
 
 
 <style scoped>
