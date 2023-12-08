9f5aec24204c278124d7afd54f1d05abaf19d17c
BDog1105 <98791807+BDog1105@users.noreply.github.com>
2023-12-07 21:20:46 -0500

    Fixed issue commuinciating with session.ts, added sign up capabilities

--- client/src/components/LoginBadge.vue
+++ client/src/components/LoginBadge.vue
@@ -1,64 +1,127 @@
 <script setup lang="ts">
-  import { getSession, useLogin } from '@/model/session'
 
-  const session = getSession()
-  const { login, logout } = useLogin()
+    import { useLogin, createUser } from '@/model/session';
+    import router from '@/router';
+    import { ref, type Ref } from 'vue';
 
-  const doLogin = () => {
-    login('hbingley1@plala.or.jp', 'CQutx25i8r')
-  }
+    const name: Ref<HTMLInputElement | undefined> = ref(undefined);
+    const email: Ref<HTMLInputElement | undefined> = ref(undefined);
+    const password: Ref<HTMLInputElement | undefined> = ref(undefined);
+        
+    let isInvalidForm = ref(false);
 
-  const doLogin1 = () => {
-    login('shaq@nba.com', 'Magic')
-  }
+    const props = defineProps<{
+        isModalActive: boolean,
+        mode: string | undefined
+    }>();
 
-  const doLogin2 = () => {
-    login('quandale@dingle', 'rehehehehe')
-  }
+    const emit = defineEmits<{
+        (e:'updateModal'): void;
+        (e:'updateMode', value: string | undefined): void;
+    }>();
 
-  const doLogout = () => {
-    logout();
-  }
+
+    const signIn = (bool: boolean) => { //handles the UI 
+        if(bool){
+            emit('updateModal');
+            router.push('/exercise/feed'); 
+        }
+    }
+
+    async function checkLogin(emailRef: string | undefined, passwordRef: string | undefined) {
+        const response = await useLogin(emailRef, passwordRef)
+            .catch(error => {
+                isInvalidForm.value = true;  
+                return;
+            });
+        if(response) {
+            signIn(true); //update the UI
+        }
+    }
+
+    const signup = async (nameRef: string, emailRef: string, passwordRef: string) => {
+        const response = await createUser(nameRef, emailRef, passwordRef)
+            .catch(error => { 
+                console.log(error);
+                return;
+            });
+
+        signIn(true); //update the UI 
+    }
+
+    const handleMode = (nameRef: string, emailRef: string, passwordRef: string) => {
+        switch(props.mode) {
+            case "sign-in":
+                checkLogin(emailRef, passwordRef);
+                break;
+            case "sign-up":
+                signup(nameRef, emailRef, passwordRef);
+                break;
+            default:
+        }
+
+        
+    }
 
 </script>
 
 <template>
-  <div class="has-text-right" v-if="session.user">
-    Welcome, {{ session.user.firstName }} {{ session.user.lastName }} <br>
-    <small>
-      {{ session.user.email }}
-      <a class="button is-small is-light is-warning" @click.prevent="doLogout">
-        <span class="icon">
-          <i class="fas fa-sign-out-alt"></i>
-        </span>
-      </a>
-    </small>
-  </div>
-  <div class="buttons" v-else>
-    <div class ="navbar-item has-dropdown is-hoverable">
-    <a class="button is-light"> 
-      Log in
-    </a>
-      <div class="navbar-dropdown">
-          <a class="navbar-item" @click.prevent="doLogin"> Sheldon </a>
-          <a class="navbar-item" @click.prevent="doLogin1"> Shaq </a>
-          <a class="navbar-item" @click.prevent="doLogin2"> Quandale </a>
-          <hr class="navbar-divider" data="">
-         <a class="navbar-item" data = ""> Other Account </a>
-      </div>
+    <!--Login screen-->
+    <div class="modal" :class="{ 'is-active': props.isModalActive }" id="signup"> 
+        <div class="modal-background"></div>
+        <div class="modal-content">
+            <form @submit.prevent="() => handleMode(name?.value as string, email?.value as string, password?.value as string)" class="box">
+
+                <div class="column field" v-if="props.mode==='sign-up'">
+                    <label for="" class="label">Name</label>
+                    <div class="control has-icons-left">
+                        <input type="name" placeholder="Name" class="input" required ref="name">
+                        <span class="icon is-small is-left">
+                            <i class="fa-solid fa-diamond"></i>
+                        </span>
+                    </div>
+                </div>
+
+                <div class="column field">
+                    <label for="" class="label">Email</label>
+                    <div class="control has-icons-left">
+                        <input type="email" placeholder="Enter your email" class="input" :class="{'is-danger': isInvalidForm}" required ref="email">
+                        <span class="icon is-small is-left">
+                            <i class="fa fa-envelope"></i>
+                        </span>
+                    </div>
+                </div>
+                <div class="column field">
+                    <label for="" class="label">Password</label>
+                    <div class="control has-icons-left">
+                        <input type="password" placeholder="Enter your password" class="input" :class="{'is-danger': isInvalidForm}" required ref="password">
+                        <span class="icon is-small is-left">
+                            <i class="fa fa-lock"></i>
+                        </span>
+                    </div>
+                    <a class="is-size-7 has-text-primary" v-show="props.mode!=='sign-up'">forgot your password?</a>
+                </div>
+                <div class="column">
+                    <div class="icon-text" v-show="isInvalidForm == true">
+                        <span class="icon">
+                            <i class='fa fa-warning is-large' style="color:red"></i>
+                        </span>
+                        <span class="has-text-danger is-size-7">Invalid Email or Password</span>
+                    </div>
+                    <button class="button is-primary is-fullwidth" type="submit" v-if="props.mode==='sign-in'">Sign In</button>
+                    <button class="button is-warning is-fullwidth" type="submit" v-else>Create Account</button>
+                </div>
+                <div class="has-text-centered">
+                    <p class="is-size-7" v-if="props.mode==='sign-in'"> Don't have an account? <a class="has-text-primary" @click="emit('updateMode', 'sign-up')">Sign up</a></p>
+                    <p class="is-size-7" v-else>Already have an account? <a class="has-text-primary" @click="emit('updateMode', 'sign-in')">Log in</a></p>
+                </div>
+            </form>
+        </div>
+
+        <button class="modal-close is-large" @click="emit('updateModal')" aria-label="close"></button>
+        <button class="js-modal-trigger is-warning" data-target="signup">
+            Signup
+        </button>
     </div>
 
-    <a class="button is-primary">
-      <strong>Sign up</strong>
-    </a>
-  </div>
-                                        
-                                    
-
 </template>
-
-
-
-<style scoped>
-
-</style>
